76f375e45619da0a7e9755e05db2427ea747d88c
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-12 08:25:39 +0500

    Time: 55 ms (75.15%), Space: 51.4 MB (80.09%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
@@ -0,0 +1,11 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    return arr.reduce((acc, item, index) => {
+        if (fn(item, index)) {
+            acc.push(item);
+        }
+
+        return acc;
+    }, [])
+};
