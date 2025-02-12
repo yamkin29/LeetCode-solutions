b7bcfd11d69db47864a1fbe67bb107413d05b10a
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-12 07:29:48 +0500

    Time: 60 ms (35.86%), Space: 51.3 MB (56.78%) - LeetHub

---
+++ 2732-counter/2732-counter.ts
@@ -0,0 +1,15 @@
+function createCounter(n: number): () => number {
+    let counter: number = -1;
+    return function() {
+        counter++;
+        return n + counter;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
