be813362dfd5c770c727e1367a5994ecc29909d4
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-12 08:11:35 +0500

    Time: 69 ms (59.64%), Space: 52.8 MB (25.68%) - LeetHub

--- 2741-function-composition/2741-function-composition.ts
+++ 2741-function-composition/2741-function-composition.ts
@@ -2,18 +2,15 @@ type F = (x: number) => number;
 
 function compose(functions: F[]): F {
     if (functions.length === 0) {
-        return function(x) {
+        return function(x: number): number {
             return x;
         }
     }
 
     return function (x: number): number {
-        let result: number = x;
-        for (let i = functions.length - 1; i >= 0; i--) {
-            result = functions[i](result);
-        }
-
-        return result;
+        return functions.reduceRight((acc, fn) => {
+            return (x: number) => fn(acc(x));
+        }, (x:number) => x)(x)
     }
 };
 
