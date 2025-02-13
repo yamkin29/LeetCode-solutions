e102981a2a621d059e6019aa32395d488cf11a31
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-13 08:25:10 +0500

    Time: 48 ms (96.72%), Space: 51.9 MB (25.89%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.ts
@@ -0,0 +1,11 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    let acc = init;
+
+    nums.forEach((item) => {
+        acc = fn(acc, item);
+    })
+
+    return acc;
+};
