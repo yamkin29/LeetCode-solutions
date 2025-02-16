8f836482ffa1551842acbffed87e712928fc2bf5
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-16 18:06:20 +0500

    Time: 61 ms (32.5%), Space: 51 MB (96.07%) - LeetHub

---
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.ts
@@ -0,0 +1,21 @@
+function* fibGenerator(): Generator<number, any, number> {
+    let a = 0;
+    let b = 1;
+
+    yield a;
+    yield b;
+
+    while(true) {
+        let next = a + b;
+        yield next;
+
+        a = b;
+        b = next;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
