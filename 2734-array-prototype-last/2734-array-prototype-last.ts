a4968f7c5bf3220904d0387cfa73300f7b8f0d79
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-18 08:26:02 +0500

    Time: 65 ms (10.32%), Space: 50.8 MB (54.22%) - LeetHub

---
+++ 2734-array-prototype-last/2734-array-prototype-last.ts
@@ -0,0 +1,18 @@
+interface Array<T> {
+    last(): T | -1;
+}
+
+Array.prototype.last = function() {
+    if (this[this.length - 1] === null) {
+        return null;
+    } else if (this.length === 0) {
+        return -1;
+    }
+
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
