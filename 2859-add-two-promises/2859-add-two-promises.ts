fa45ba8b5cb03d735d0398f44e89cc7e97e39dbd
Aleksey Yamkin <me@jamkin.ru>
2025-02-27 21:05:00 +0500

    Resolve task 2859.

--- 2859-add-two-promises/2859-add-two-promises.ts
+++ 2859-add-two-promises/2859-add-two-promises.ts
@@ -2,7 +2,7 @@ type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
     return await promise1 + await promise2;
-};
+}
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
